b33cb954a0bc9f50cd976790b6e3cfea07b44fbe
AferDust <aferdust27@gmail.com>
2024-04-14 12:00:12 +0500

    feat: adding bank_name filed to card serializer

--- api/modules/user_card/serializers.py
+++ api/modules/user_card/serializers.py
@@ -7,27 +7,26 @@ from api.models import Card
 class UserCoverSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ("first_name", "last_name")
+        fields = ["first_name", "last_name"]
 
 
 class CardSerializer(serializers.ModelSerializer):
-    # user = UserCoverSerializer(read_only=True)
+    bank_name = serializers.CharField(source='card_type.bank.name')
     card_type_name = serializers.SerializerMethodField(read_only=True)
     name = serializers.SerializerMethodField(read_only=True)
 
-
     class Meta:
         model = Card
-        fields = (
-        'id', 'number', 'expired_date', 'card_type', "card_type_name", "name")  # 'user' is deliberately excluded
-        read_only_fields = ('id',)
+        fields = ['id', 'number', 'bank_name', 'expired_date',
+                  'card_type', "card_type_name", "name"]
+        read_only_fields = ['id']
 
     def get_card_type_name(self, obj):
         return obj.card_type.name
+
     def get_name(self, obj):
         return obj.user.first_name + " " + obj.user.last_name
 
     def create(self, validated_data):
-        # Automatically assign the logged-in user as the owner of the card
         validated_data['user'] = self.context['request'].user
         return super().create(validated_data)
