a97248a012485d34214833762006acce10bda8a0
Nurkhat Muratkan <nurhatmurathan001@gmail.com>
2024-04-14 08:16:10 +0500

    fix: urls

--- api/modules/cashback/urls.py
+++ api/modules/cashback/urls.py
@@ -12,7 +12,8 @@ bank_card_type_readonly_router = routers.SimpleRouter()
 bank_card_type_readonly_router.register(r'', BankCardReadonlyModelViewSet, basename='cashback')
 
 urlpatterns = [
-    path('', include(bank_card_type_readonly_router.urls)),
     path('create/', CashbackCreateAPIView.as_view()),
     path('list/<int:category_id>/', CategoryCashbackAPIView.as_view()),
+    path('', include(bank_card_type_readonly_router.urls)),
+
 ]
